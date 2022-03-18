03638aace0bcd8accd76e02d2947024fc2b802fd
venandi48 <bow999@naver.com>
2022-03-18 18:30:43 +0900

    220318 db hw commit

---
+++ hw/220318_hw.sql
@@ -0,0 +1,228 @@
+
+-- ====================================================
+-- 1. 다중열 서브쿼리
+-- 2. 220317 과제 JOIN문제 oracle 문법으로 변환
+-- ====================================================
+-------------------------------------------------------
+-- 1. 다중열 서브쿼리
+-------------------------------------------------------
+
+-- #1번
+-- 부서별 최대급여를 받는 사원의 사원명, 부서명, 급여를 출력. 
+select
+    e.emp_name 사원명,
+    d.dept_title 부서명,
+    to_char(salary, 'fm999,999,999') 급여
+from employee e
+    join department d
+        on e.dept_code = d.dept_id
+where
+    salary in (
+        select
+            max(salary)
+        from employee
+        group by dept_code
+        );
+
+
+-- #심화1
+-- 최소급여를 받는 사원도 출력.
+select
+    e.emp_name 사원명,
+    d.dept_title 부서명,
+    to_char(salary, 'fm999,999,999') 급여
+from employee e
+    join department d
+        on e.dept_code = d.dept_id
+where
+    (dept_code, salary) in (
+            select dept_code, max(salary) from employee group by dept_code
+            union
+            select dept_code, min(salary) from employee group by dept_code
+        );
+-- 풀이2
+--where
+--    salary in (select max(salary) from employee group by dept_code)
+--    or salary in (select min(salary) from employee group by dept_code);
+
+
+
+-- #심화2
+-- 인턴사원도 포함시키기
+select
+    e.emp_name 사원명,
+    nvl(d.dept_title, '인턴') 부서명,
+    to_char(salary, 'fm999,999,999') 급여
+from employee e
+    left join department d
+        on e.dept_code = d.dept_id
+where
+    (nvl(dept_code,'D0'), salary) in (
+            select nvl(dept_code, 'D0'), max(salary) from employee group by dept_code
+            union
+            select nvl(dept_code, 'D0'), min(salary) from employee group by dept_code
+        );
+
+
+-------------------------------------------------------
+-- 2. 220317 과제 JOIN문제 oracle 문법으로 변환
+-------------------------------------------------------
+
+-- #2번
+-- 주민번호가 70년대 생이면서 성별이 여자이고, 성이 전씨인 직원들의 
+-- 사원명, 주민번호, 부서명, 직급명을 조회하시오.
+select
+    emp_name 사원명, emp_no 주민번호, dept_title 부서명, job_name 직급명
+from
+    employee e, department d, job j
+where
+    e.dept_code = d.dept_id(+)
+    and e.job_code = j.job_code
+    and substr(emp_no, 1, 1) = 7
+    and substr(emp_no, 8, 1) = 2
+    and emp_name like '전%';
+
+
+-- #3번
+-- 가장 나이가 적은 직원의 사번, 사원명, 나이, 부서명, 직급명을 조회하시오.
+select
+    emp_id 사번,
+    emp_name 사원명, 
+    extract(year from sysdate) -
+        (decode(substr(emp_no,8,1),'1',1900,'2',1900,2000)) - substr(emp_no,1,2) + 1 나이,
+    d.dept_title 부서명,
+    j.job_name 직급명
+from
+    employee e,
+    department d,
+    job j,
+    (
+        select 
+            min(extract(year from sysdate) -
+            (decode(substr(emp_no,8,1),'1',1900,'2',1900,2000)) - substr(emp_no,1,2) + 1) min_age
+        from employee
+    ) tb_min_age
+where
+    e.dept_code = d.dept_id(+)
+    and e.job_code = j.job_code
+    and tb_min_age.min_age
+        = extract(year from sysdate) -
+        (decode(substr(emp_no,8,1),'1',1900,'2',1900,2000)) - substr(emp_no,1,2) + 1;
+
+
+-- #4번
+-- 이름에 '형'자가 들어가는 직원들의 사번, 사원명, 부서명을 조회하시오.
+select
+    e.emp_id 사번, e.emp_name 사원명, d.dept_title 부서명
+from employee e, department d
+where
+    e.dept_code = d.dept_id(+)
+    and e.emp_name like '%형%';
+
+
+-- #5번
+-- 해외영업팀에 근무하는 사원명, 직급명, 부서코드, 부서명을 조회하시오.
+select
+    e.emp_name 사원명, j.job_name 직급명, d.dept_id 부서코드, d.dept_title 부서명
+from
+    employee e, job j, department d
+where 
+    e.job_code = j.job_code
+    and e.dept_code = d.dept_id(+)
+    and d.dept_title like '해외영업%';
+
+
+-- #6번
+-- 보너스포인트를 받는 직원들의 사원명, 보너스포인트, 부서명, 근무지역명을 조회하시오.
+select
+    e.emp_name 사원명,
+    e.bonus 보너스포인트,
+    nvl(d.dept_title, ' - ') 부서명,
+    nvl(l.local_name, ' - ') 근무지역명
+from
+    employee e, department d, location l
+where 
+    e.dept_code = d.dept_id(+)
+    and d.location_id = l.local_code(+)
+    and e.bonus is not null;
+
+
+-- #7번
+-- 부서코드가 D2인 직원들의 사원명, 직급명, 부서명, 근무지역명을 조회하시오.
+select
+    e.emp_name 사원명,
+    j.job_name 직급명,
+    d.dept_title 부서명,
+    l.local_name 근무지역명
+from
+    employee e, job j, department d, location l
+where
+    e.job_code = j.job_code
+    and e.dept_code = d.dept_id
+    and d.location_id = l.local_code
+    and e.dept_code = 'D2';
+
+
+-- #8번
+-- 급여등급테이블의 등급별 최대급여(MAX_SAL)보다 많이 받는 직원들의 사원명, 직급명, 급여, 연봉을 조회하시오.
+-- (사원테이블과 급여등급테이블을 SAL_LEVEL컬럼기준으로 동등 조인할 것)
+select
+    e.emp_name 사원명,
+    j.job_name 직급명,
+    to_char(e.salary, 'fml999,999,999') 급여,
+    to_char((e.salary + (e.salary * nvl(e.bonus, 0))) * 12, 'fml999,999,999') 연봉
+from
+    employee e, job j, sal_grade s
+where
+    e.job_code = j.job_code
+    and s.sal_level = e.sal_level
+    and e.salary > s.max_sal;
+
+
+-- #9번
+-- 한국(KO)과 일본(JP)에 근무하는 직원들의 
+-- 사원명, 부서명, 지역명, 국가명을 조회하시오.
+select
+    e.emp_name 사원명, d.dept_title 부서명, l.local_name 지역명, n.national_name 국가명
+from
+    employee e, department d, location l, nation n
+where 
+    e.dept_code = d.dept_id(+)
+    and d.location_id = l.local_code(+)
+    and l.national_code = n.national_code(+)
+    and l.national_code in ('KO', 'JP');
+
+
+-- #10번
+-- 같은 부서에 근무하는 직원들의 사원명, 부서코드, 동료이름을 조회하시오.
+-- self join 사용
+select 
+    e.emp_name 사원명, e.dept_code 부서코드, co_e.emp_name 동료이름
+from
+    employee e, employee co_e, department d
+where
+    e.dept_code = co_e.dept_code
+    and e.dept_code = d.dept_id(+)
+    and e.emp_name != co_e.emp_name
+order by e.emp_name;
+
+
+-- #11번
+-- 보너스포인트가 없는 직원들 중에서 직급이 차장과 사원인 직원들의 사원명, 직급명, 급여를 조회하시오.
+select
+    e.emp_name 사원명,
+    j.job_name 직급명,
+    to_char(e.salary, 'fml999,999,999') 급여
+from employee e, job j
+where
+    e.job_code = j.job_code
+    and e.bonus is null and j.job_name in ('차장', '사원');
+
+
+-- #12번
+-- 재직중인 직원과 퇴사한 직원의 수를 조회하시오.
+select
+    decode(quit_yn,'N','재직자','퇴사자') 재직여부,
+    count(*) 인원수
+from employee
+group by quit_yn;
---
+++ hw_chun/oracle_chun_220318.sql
@@ -0,0 +1,100 @@
+-- ====================================================
+-- 실습문제 chun 계정
+-- ====================================================
+
+select * from tb_department; -- 학과
+select * from tb_student; -- 학생
+    -- (tb_department.department_no = tb_student.department_no)
+    -- (tb_professor.professor_no = tb_student.coach_professor_no)
+select * from tb_professor; -- 교수
+    -- (tb_department.department_no = tb_professor.department_no)
+select * from tb_class; -- 수업
+    -- (tb_department.department_no = tb_class.department_no)
+select * from tb_class_professor; -- 수업-교수
+    -- (tb_class.class_no = tb_class_professor.class_no)
+    -- (tb_professor.professor_no = tb_class_professor.professor_no)
+select * from tb_grade; -- 학점
+    -- (tb_student.student_no = tb_grade.student_no)
+    -- (tb_class.class_no = tb_grade.class_no)
+
+-------------------------------------------------------
+
+-------------------------------------------------------
+-- 220318 실습문제 : INNER JOIN & OUTER JOIN
+-------------------------------------------------------
+
+-- #1번
+-- 학번, 학생명, 담당교수명을 출력하세요.
+-- 담당교수가 없는 학생은 '없음'으로 표시
+select
+    s.student_no 학번,
+    s.student_name 학생명,
+    nvl(p.professor_name, '없음') 담당교수명
+from
+    tb_student s left join tb_professor p
+            on s.coach_professor_no = p.professor_no;
+
+
+-- #2번
+-- 학과별 교수명과 인원수를 모두 표시하세요.
+select 
+    d.department_name 학과,
+    nvl(p.professor_name, ' - ') 교수명,
+    nvl(tb_count.pro_count, 0) 학과교수인원수
+from 
+    tb_department d
+        left join tb_professor p
+            on d.department_no = p.department_no
+        left join (
+                    select
+                        department_no,
+                        count(professor_name) pro_count
+                    from tb_professor
+                    group by department_no
+                ) tb_count
+            on d.department_no = tb_count.department_no
+order by d.department_name;
+
+
+-- #3번
+-- 이름이 [~람]인 학생의 평균학점을 구해서 학생명과 평균학점(반올림해서 소수점둘째자리까지)과 같이 출력.
+-- (동명이인일 경우에 대비해서 student_name만으로 group by 할 수 없다.)
+select
+    s.student_name 학생명,
+    s.student_no 학번,
+    to_char(round(tb_avg.avg_point, 2), '0.00') 평균학점
+from
+    tb_student s join (
+            select
+                g.student_no avg_student_no,
+                avg(g.point) avg_point
+            from tb_grade g
+            group by g.student_no) tb_avg
+        on s.student_no = tb_avg.avg_student_no
+where s.student_name like '%람'
+order by s.student_name;
+
+
+-- #4번
+-- 학생별 다음정보를 구하라.
+/*
+--------------------------------------------
+학생명  학기     과목명    학점
+-------------------------------------------
+감현제    200401    전기생리학     4.5
+            .
+            .
+--------------------------------------------
+*/
+select
+    s.student_name 학생명,
+    g.term_no 학기,
+    c.class_name 과목명,
+    g.point 학점
+from
+    tb_student s
+        join tb_grade g
+            on s.student_no = g.student_no
+        join tb_class c
+            on g.class_no = c.class_no
+order by 1, 2, 3;
